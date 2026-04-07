4df883f0bae5ec716add8c7a63b6c15a9876e466
H-YUN-SS <920563885@qq.com>
2026-04-07 14:35:29 +0800

    refactor: restructure component logic

--- YLruCache.h
+++ YLruCache.h
@@ -10,110 +10,126 @@
 
 namespace YCache
 {
+    template<typename Key, typename Value> class YLruCache;
+
     template<typename Key,typename Value>
     class LruNode
     {
-        private:
+    private:
         Key key_;
         Value value_;
-        size_t accessCount_;//访问次数
-        std::weak_ptr<LruNode>prev_;//上一个节点的指针 弱指针
+        size_t accessCount_;
+        std::weak_ptr<LruNode> prev_;//上一个节点的指针 弱指针
         std::shared_ptr<LruNode> next_;
-        public:
-        LruNode(Key key,Value value)
-        :key_(key)
-        ,value_(value)
-        ,accessCount_(1)
+    public:
+        LruNode(Key key, Value value)
+            : key_(key)
+            , value_(value)
+            , accessCount_(1)
         {}
         Key getKey()const{return key_;}
         Value getValue()const{return value_;}
         void setValue(const Value& value){value_=value;}
-        size_t getAccessCount()const{return accessCount_;}
-        void incrementAccessCount(){accessCount_++;}
-        template<typename K,typename V>
-        friend class YLruCache;
+        size_t getAccessCount() const { return accessCount_; } 
+        void incrementAccessCount() { ++accessCount_; } 
+
+        friend class YLruCache<Key, Value>;
     };
 
     template<typename Key, typename Value>
     class YLruCache : public YICachePolicy<Key,Value>
     {
-        public:
-            //给 LruNode<Key, Value> 起一个简短别名：LruNodeType
-            using LruNodeType = LruNode<Key, Value>;
-            //NodePtr = 智能指针，用来安全管理节点内存，不崩溃
-            using NodePtr = std::shared_ptr<LruNodeType>;
-            //NodeMap = 哈希表key → 快速找到对应的节点
-            using NodeMap = std::unordered_map<Key, NodePtr>;
-            explicit YLruCache(int capacity)
-            :capacity_(capacity)
+    public:
+        //给 LruNode<Key, Value> 起一个简短别名：LruNodeType
+        using LruNodeType = LruNode<Key, Value>;
+        //NodePtr = 智能指针，用来安全管理节点内存，不崩溃
+        using NodePtr = std::shared_ptr<LruNodeType>;
+        //NodeMap = 哈希表key → 快速找到对应的节点
+        using NodeMap = std::unordered_map<Key, NodePtr>;
+        
+        explicit YLruCache(int capacity)
+            : capacity_(capacity)
+        {
+            initializeList();
+        }
+
+        ~YLruCache()override=default;
+        
+        void put(Key key, Value value) override
+        {
+            if (capacity_ <= 0)
+                return;
+        
+            std::lock_guard<std::mutex> lock(mutex_);
+            auto it = nodeMap_.find(key);
+            if (it != nodeMap_.end())
             {
-                initializeList();
+                // 如果在当前容器中,则更新value,并调用get方法，代表该数据刚被访问
+                updateExistingNode(it->second, value);
+                return ;
             }
 
-            ~YLruCache()override=default;
+            addNewNode(key, value);
+        }
+        
+        bool get(Key key, Value& value) override;
+        Value get(Key key) override;
+        void remove(Key key);
 
-            void put(Key key,Value value) override;
-            bool get(Key key,Value& value) override;
-            Value get(Key key) override;
+    private:
+        void initializeList();
+        //把一个节点，移动到链表最末尾 = 标记为 “最近刚用过”
+        void moveToMostRecent(NodePtr node);
+        void insertNode(NodePtr node);       
+        void removeNode(NodePtr node);     
+        void evictLeastRecent();  
+        void updateExistingNode(NodePtr node, const Value& value);
+        void addNewNode(const Key& key, const Value& value);
 
-        private:
-            void initializeList();
-            //把一个节点，移动到链表最末尾 = 标记为 “最近刚用过”
-            void moveToMostRecent(NodePtr node);
-
-            void insertNode(NodePtr node);       
-            void removeNode(NodePtr node);     
-            void evictLeastRecent();  
-
-            int capacity_;
-            NodeMap nodeMap_;
-            std::mutex mutex_;
-            NodePtr dummyHead_;
-            NodePtr dummyTail_;
+        int capacity_;
+        NodeMap nodeMap_;
+        std::mutex mutex_;
+        NodePtr dummyHead_;
+        NodePtr dummyTail_;
     };
+
     template<typename Key,typename Value>
-    void  YLruCache<Key,Value>::initializeList()
+    void YLruCache<Key,Value>::initializeList()
     {
-        dummyHead_=std::make_shared<LruNodeType>(Key(),Value());
-        dummyTail_=std::make_shared<LruNodeType>(Key(),Value());
-        dummyHead_->next_=dummyTail_;
-        dummyTail_->prev_=dummyHead_;
+        dummyHead_ = std::make_shared<LruNodeType>(Key(), Value());
+        dummyTail_ = std::make_shared<LruNodeType>(Key(), Value());
+        dummyHead_->next_ = dummyTail_;
+        dummyTail_->prev_ = dummyHead_;
     }
+
     template<typename Key,typename Value>
     void YLruCache<Key,Value>::removeNode(NodePtr node)
     {
-        if(node->prev_.expired()||!node->next_)
+        // 1111：对齐原仓库的条件判断逻辑（原仓库是“满足条件才执行”，而非“不满足则返回”）
+        if (!node->prev_.expired() && node->next_)
         {
-            return;
+            auto prevNode = node->prev_.lock();
+            prevNode->next_ = node->next_;
+            node->next_->prev_ = prevNode;
+            node->next_ = nullptr;
         }
-        auto prevNode=node->prev_.lock();
-        prevNode->next_=node->next_;
-        node->next_->prev_=prevNode;
-        node->next_=nullptr;
-        node->prev_.reset();
     }
+
     template<typename Key,typename Value>
     void YLruCache<Key,Value>::evictLeastRecent()
     {
-        NodePtr oldestNode=dummyHead_->next_;
-        if(oldestNode==dummyTail_)
-        {
-            return;
-        }
-        removeNode(oldsoldestNodetNode);
-        NodeMap_.erase(oldestNode->getKey());
+        NodePtr oldestNode = dummyHead_->next_;
+        removeNode(oldestNode);
+        nodeMap_.erase(oldestNode->getKey());
     }
+
     template<typename Key,typename Value>
     void YLruCache<Key,Value>::insertNode(NodePtr node)
     {
-        if (!node || !dummyTail_) return;
-            node->prev_ = dummyTail_->prev_;
-            node->next_ = dummyTail_;
-
-            auto prev_node = dummyTail_->prev_.lock();
-            prev_node->next_ = node;
-
-            dummyTail_->prev_ = node;
+        node->next_ = dummyTail_;
+        node->prev_ = dummyTail_->prev_;
+        dummyTail_->prev_.lock()->next_ = node; 
+        dummyTail_->prev_ = node;
     }
 
     template<typename Key,typename Value>
@@ -122,24 +138,18 @@ namespace YCache
         removeNode(node);
         insertNode(node);
     }
-    
-    
+
     template<typename Key,typename Value>
-    void YLruCache<Key,Value>::put(Key key,Value value)
+    void YLruCache<Key,Value>::updateExistingNode(NodePtr node, const Value& value)
     {
-        if(capacity_<=0)
-        {
-            return ;
-        }
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto it=nodeMap_.find(key);
-        if(it!=nodeMap_.end())
-        {
-            it->second->setValue(value);
-            moveToMostRecent(it->second);
-            return;
-        }
-        if(nodeMap_.size()>=capacity_)
+        node->setValue(value);
+        moveToMostRecent(node);
+    }
+
+    template<typename Key,typename Value>
+    void YLruCache<Key,Value>::addNewNode(const Key& key, const Value& value)
+    {
+        if (nodeMap_.size() >= capacity_)
         {
             evictLeastRecent();
         }
@@ -147,31 +157,39 @@ namespace YCache
         NodePtr newNode = std::make_shared<LruNodeType>(key, value);
         insertNode(newNode);
         nodeMap_[key] = newNode;
-
-
     }
 
     template<typename Key,typename Value>
-    bool YLruCache<Key,Value>::get(Key key,Value& value)
+    bool YLruCache<Key,Value>::get(Key key, Value& value)
     {
         std::lock_guard<std::mutex> lock(mutex_);
-
-        auto it = NodeMap_.find(key);
-        if (it == NodeMap_.end()) {
-            return false;
+        auto it = nodeMap_.find(key);
+        if (it != nodeMap_.end())
+        {
+            moveToMostRecent(it->second);
+            value = it->second->getValue();
+            return true;
         }
-
-
-        moveToMostRecent(it->second);
-        value = it->second->getValue();
-        return true;
+        return false;
     }
 
     template<typename Key,typename Value>
     Value YLruCache<Key,Value>::get(Key key)
     {
-        Value val{};
-        get(key, val);
-        return val;
+        Value value{};
+        get(key, value);
+        return value;
+    }
+
+    template<typename Key,typename Value>
+    void YLruCache<Key,Value>::remove(Key key)
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        auto it = nodeMap_.find(key);
+        if (it != nodeMap_.end())
+        {
+            removeNode(it->second);
+            nodeMap_.erase(it);
+        }
     }
 }
